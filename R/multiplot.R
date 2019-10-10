5eb93e02473e6f802f15e3fc91d6336763ba9e3c
Leslie Emery <emeryl@uw.edu>
2019-10-10 15:33:17 -0700

    Fix linting errors

--- R/multiplot.R
+++ R/multiplot.R
@@ -8,7 +8,8 @@
 # 3 will go all the way across the bottom.
 #
 # @param cols The number of columns in the plot layout
-# @param layout A matrix specifying the layout of the plots. If present, 'cols' is ignored
+# @param layout A matrix specifying the layout of the plots. If present,
+#   'cols' is ignored
 # @import grid
 # @return NULL (prints plot to active device)
 # @export
@@ -17,18 +18,18 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     # Make a list from the ... arguments and plotlist
     plots <- c(list(...), plotlist)
 
-    numPlots = length(plots)
+    num_plots <- length(plots)
 
     # If layout is NULL, then use 'cols' to determine layout
     if (is.null(layout)) {
         # Make the panel
         # ncol: Number of columns of plots
         # nrow: Number of rows needed, calculated from # of cols
-        layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                         ncol = cols, nrow = ceiling(numPlots/cols))
+        layout <- matrix(seq(1, cols * ceiling(num_plots / cols)),
+                         ncol = cols, nrow = ceiling(num_plots / cols))
     }
 
-    if (numPlots==1) {
+    if (num_plots == 1) {
         print(plots[[1]])
 
     } else {
@@ -37,8 +38,8 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
         pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
 
         # Make each plot, in the correct location
-        for (i in 1:numPlots) {
-            # Get the i,j matrix positions of the regions that contain this subplot
+        for (i in 1:num_plots) {
+            # Get the i,j matrix positions of the regions containing the subplot
             matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
 
             print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
